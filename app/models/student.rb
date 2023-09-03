50b91672fee3c7448602b47096c86053cba71f14
LincolinAngaya <lincolinangaya5626@gmail.com>
2023-09-03 15:51:12 +0300

    add validation to the nparams

--- app/models/student.rb
+++ app/models/student.rb
@@ -2,5 +2,8 @@
 
 class Student < ApplicationRecord
     # Your model code here
+    validates :first_name, presence: true, length: { maximum: 255 }
+    validates :last_name, presence: true, length: { maximum: 255 }
+    validates :email, presence: true, length: { maximum: 255 }, format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i }, uniqueness: true
   end
   
