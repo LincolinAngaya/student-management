54039524bf65e1bbca8d304799241cb62d2610e8
LincolinAngaya <lincolinangaya5626@gmail.com>
2023-09-03 15:38:35 +0300

    code modified

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -11,15 +11,28 @@ class StudentsController < ApplicationController
         @student = Student.new
     end
 
+    # def create
+    #     @student = Student.new(
+    #         first_name: params[:student][first_name],
+    #         last_name: params[:student][last_name],
+    #         email: params[:student][email]
+    #     )
+
+    #     @student.save
+    # end
     def create
         @student = Student.new(
-            first_name: params[:student][first_name],
-            last_name: params[:student][last_name],
-            email: params[:student][email]
+          first_name: params[:student][:first_name],
+          last_name: params[:student][:last_name],
+          email: params[:student][:email]
         )
-
-        @student.save
-    end
-
+      
+        if @student.save
+          redirect_to students_path, notice: 'Student was successfully created.'
+        else
+          render 'new'
+        end
+      end
+      
   end
   
