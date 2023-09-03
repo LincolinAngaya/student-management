02f1555daa6088850837bfbf7a54b6a09a771693
LincolinAngaya <lincolinangaya5626@gmail.com>
2023-09-03 15:47:16 +0300

    change the create code replace with student params

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -21,11 +21,7 @@ class StudentsController < ApplicationController
     #     @student.save
     # end
     def create
-        @student = Student.new(
-          first_name: params[:student][:first_name],
-          last_name: params[:student][:last_name],
-          email: params[:student][:email]
-        )
+        @student = Student.new(student_params)
       
         if @student.save
           redirect_to students_path, notice: 'Student was successfully created.'
