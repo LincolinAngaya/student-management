1f818bf4f220810e9c3a8625bbb6a1e3c77e5588
LincolinAngaya <lincolinangaya5626@gmail.com>
2023-09-03 16:46:04 +0300

    done updating the student records

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -16,6 +16,19 @@ class StudentsController < ApplicationController
         @student =Student.find(params[:id])
 
     end
+
+    def update    
+        @student =Student.find(params[:id])
+
+            if  @student.update(student_params)
+                redirect_to students_path
+            else
+            render :edit
+            end
+          
+    end 
+
+
     def new 
         @student = Student.new
     end
