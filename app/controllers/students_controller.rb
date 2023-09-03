1b56a0f311a9a150c6a0b1d27ea531a556e243f1
LincolinAngaya <lincolinangaya5626@gmail.com>
2023-09-03 15:34:26 +0300

    add new action to save the paramaeters

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -11,5 +11,13 @@ class StudentsController < ApplicationController
         @student = Student.new
     end
 
+    def create
+        @student = Student.new(
+            first_name: params[:student][first_name],
+            last_name: params[:student][last_name],
+            email: params[:student][email]
+        )
+    end
+
   end
   
