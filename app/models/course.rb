b8202523f4af656b8a4692d382f5faf00166fa7a
LincolinAngaya <lincolinangaya5626@gmail.com>
2023-09-05 18:31:39 +0300

    add validation to your course

--- app/models/course.rb
+++ app/models/course.rb
@@ -1,3 +1,8 @@
 class Course < ApplicationRecord
+      # Validation for name
+  validates :name, presence: true, length: { maximum: 255 }
+
+  # Validation for description
+  validates :description, presence: true
 end
 
