00f906fc0edd8a29a5bd074087c7097cc2807e65
LincolinAngaya <lincolinangaya5626@gmail.com>
2023-09-07 17:25:54 +0300

    added display_greeting

--- app/models/student.rb
+++ app/models/student.rb
@@ -7,7 +7,13 @@ class Student < ApplicationRecord
     validates :email, presence: true, length: { maximum: 255 }, format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i }, uniqueness: true
 
 
+before_create :display_greetings  
 
+def display_greetings 
+
+  puts 'Hello, you executed before action callback!'
+  
+end 
 
 
 
